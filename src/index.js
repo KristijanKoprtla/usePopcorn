796202f4c69342c48e202708da45c90ee4264613
BombMarley <kile146@gmail.com>
2023-06-13 22:29:29 +0200

    Improve reusability with props

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 
 import StarRating from "./StarRating";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rating {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating
+      maxRating={10}
+      color="blue"
+      size={20}
+      classname="test"
+      defaultRating={3}
+    />
+
+    <Test />
   </React.StrictMode>
 );
